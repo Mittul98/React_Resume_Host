68faaf219072c306f3d27dc7dfc8859f1fc768f4
Mittul98 <mittultrivedi98@gmail.com>
2025-02-20 13:43:12 -0500

    Updated for GitHub Pages deployment

--- backend/server.js
+++ backend/server.js
@@ -43,5 +43,5 @@ const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.get('/', (req, res) => {
-    res.send('Backend is running! Use /auth/github/callback for authentication.');
-  });
+  res.send('Backend is running! Use /auth/github/callback for authentication.');
+});
